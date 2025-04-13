96b9919f0c02dd5b2e493f7cd7057466d10951a4
n0nia <denisaboriceanu63@gmail.com>
2025-04-13 11:43:44 +0300

    Testing state

--- src/components/task-card/TaskCard.jsx
+++ src/components/task-card/TaskCard.jsx
@@ -5,32 +5,34 @@ import DateContainer from '../date-container/DateContainer';
 
 function TaskCard(props) {
 
-    const [taskId, setTaskId] = useState(props.id)
-    //const state = useState(props.id);
-    //const taskId = state[0];
-    //const setTaskId = state[1];
+    const [counter, setCounter] = useState(0);
+
+    let nr = 0;
+
+    console.log("RENDER " + counter + " " + props.id);
+    console.log("NR " + nr);
 
     const handleClick = () => {
-        setTaskId("Clicked!");
-        console.log(taskId);
+        setCounter(counter + 1);
+        console.log("cnt ", counter);
+        nr = nr + 1;
     }
 
     return (
         <div className='card-wrapper'>
             <div className="card-header">
-                <p className="task-id">{taskId}</p>
+                <p className="task-id">{props.id}</p>
                     <Badge status={props.status} />
             </div>
             <div className="card-content">
-                <p>{props.name}</p>
+               {/* <p>{props.name}</p> */}
+               <p>{counter}</p>
             </div>
             <div className="card-footer">
-                <button onClick={
-                    handleClick
-                }>
-                    Click me!
+                <button onClick={handleClick}>
+                    INCREMENT
                 </button>
-                <DateContainer date={props.dueDate} />
+               {/* <DateContainer date={props.dueDate} /> */}
             </div>
         </div>
     );
