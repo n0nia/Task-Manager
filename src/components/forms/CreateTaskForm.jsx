896e07f3fb46d70ccb19ce2321027e075d411eee
dboriceanu <denisa.boriceanu@ubisoft.com>
2025-04-15 08:19:30 +0300

    Controlled Inputs Form

--- src/components/forms/CreateTaskForm.jsx
+++ src/components/forms/CreateTaskForm.jsx
@@ -3,61 +3,38 @@ import './CreateTaskForm.css'
 
 const CreateTaskForm = () => {
 
-    //const [taskName, setTaskName] = useState("");
-    //const [dueDate, setDueDate] = useState();
-    //const [taskDetails, setTaskDetails] = useState("");
+    const [taskName, setTaskName] = useState("Test");
+    const [dueDate, setDueDate] = useState();
+    const [taskDetails, setTaskDetails] = useState("");
 
-    const [formData, setFormData] = useState({
-        taskName: "",
-        dueDate: "",
-        taskDetails: ""
-    });
-/*
+
     const handleNameChange = (event) => {
-        setFormData((prevState) => ({
-                ...prevState,
-                taskName: event.target.value
-        }));
-        //setTaskName(event.target.value);
+        setTaskName(event.target.value);
     };
 
     const handleDateChange = (event) => {
-        setFormData((prevState) => ({
-            ...prevState,
-            dueDate: event.target.value
-        }));
-        //setDueDate(event.target.value);
+        setDueDate(event.target.value);
     };
 
     const handleDetailsChange = (event) => {
-        setFormData((prevState) => ({
-            ...prevState,
-            taskDetails: event.target.value
-        }));
-        //setTaskDetails(event.target.value);
+        setTaskDetails(event.target.value);
     };
-*/
-    const handleInputChange = (event) => {
-        console.log(event.target.name);
-
-        setFormData((prevState) => ({
-            ...prevState,
-            [event.target.name]: event.target.value
-            //ex: ["taskName"]: event.target.value
-        }));
-    }
 
     const handleSubmit= (event) => {
         event.preventDefault();
-        console.log("FormData: ", formData);
 
-        //const newTask = {
-        //    name: taskName,
-        //    dueDate: dueDate,
-        //    taskDetails: taskDetails
-        //};
+        const newTask = {
+            name: taskName,
+            dueDate: dueDate,
+            taskDetails: taskDetails,
+            status: "Todo"
+        };
 
-        //console.log("newTask", newTask);
+        console.log("newTask=", newTask);
+        setTaskName("");
+        setDueDate("");
+        setTaskDetails("");
+
     }
 
     return (
@@ -65,15 +42,21 @@ const CreateTaskForm = () => {
             <form onSubmit={handleSubmit}>
                 <div className="form-row">
                     <label className="label-md">Task Name</label>
-                    <input name='taskName' onChange={handleInputChange} className="input-primary" type="text" />
+                    <input value={taskName} 
+                    name='taskName' onChange={handleInputChange} 
+                    className="input-primary" type="text" />
                 </div>
                 <div className="form-row">
                     <label className="label-md">Due Date</label>
-                    <input name='dueDate' onChange={handleInputChange} className="input-primary" type="date" />
+                    <input value={dueDate}
+                    name='dueDate' onChange={handleInputChange} 
+                    className="input-primary" type="date" />
                 </div>
                 <div className="form-row">
                     <label className="label-md">Task Details</label>
-                    <textarea name='taskDetails' onChange={handleInputChange} className="input-primary" cols="30" rows="10"></textarea>
+                    <textarea value={taskDetails}
+                    name='taskDetails' onChange={handleInputChange} 
+                    className="input-primary" cols="30" rows="10"></textarea>
                 </div>
 
                 <button className="button-primary" type="submit">Create Task</button>
